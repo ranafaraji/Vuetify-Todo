b38d70204ff35e0da2bcf5f4e97037ac3a5cb33b
rana <rana@pop-os.localdomain>
2021-01-17 20:38:57 +0800

    push to github

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>To-do lists don't need to be complicated—plenty of people use 
+      a pen and paper for the job without any problem. 
+      And yet a new to-do list app seems to come out every day.
+       Why? Because keeping track of your tasks is an intensely personal thing, 
+       and people will reject anything that doesn't feel right pretty much instantly.
+        That makes it hard to find the right app.</p>
+ <p>To that end, we've been hard at work researching the best to-do apps, 
+     trying to find the right ones for various use cases.
+    Research for these pieces was exhaustive.
+    We started by finding the best apps for every platform: Android, Windows,
+      macOS, and iPhone/iPad. We then tried the top-rated apps in every respective 
+     app store, and spent way too much time migrating our personal
+   to-do lists from one app to another.</p>
+        <p>And now we're offering you what we feel is the cream of the crop.
+           Whatever you're looking for, one of these apps is going to be right for you. 
+           Click on any app to learn more about why we chose it, 
+           or keep reading for more context on to-do list apps.</p>
   </div>
 </template>
